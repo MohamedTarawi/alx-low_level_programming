d96e3f53a02931f8289281f9efe20165d456129a
MohamedTarawi <sadaqali1923@gmail.com>
2022-06-30 15:13:54 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main-entry point
  *
- * return: 0 number
- */
+ * Return: 0 number
+ * */
 int main(void)
-{	
-	puts("/"Programming is like building a multilingual puzzle");
-	return(0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
