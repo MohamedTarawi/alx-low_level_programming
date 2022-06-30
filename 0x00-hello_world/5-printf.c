7c8c56f0218b6e2cb6b592dca643597a7f169905
MohamedTarawi <sadaqali1923@gmail.com>
2022-06-30 15:48:56 -0700

    print using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main-entry point
- *
- * return: 0 number
- */
+* main - Entry point
+*
+* Return: 0 number
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
