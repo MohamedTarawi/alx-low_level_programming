25ebb48d31d2d5d08d9d348e70a1350507616e86
MohamedTarawi <sadaqali1923@gmail.com>
2022-07-04 01:59:34 -0700

    except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - alphabet in lower case except e and q
- *
- *return: always success
- */ 
+* main - alphabet in lower case except e and q
+*
+*return: always success
+*/ 
 int main(void)
 {
-       char(letter);
-	for (letter = 'a'; letter <='z'; letter++)
+	char(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 	if (letter != 'e' && letter != 'q')
 		putchar(letter);
