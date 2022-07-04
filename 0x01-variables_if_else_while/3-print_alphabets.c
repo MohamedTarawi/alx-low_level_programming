d4cf1a61681565f79dd069f93c3c83d3eff6cf16
MohamedTarawi <sadaqali1923@gmail.com>
2022-07-03 23:10:03 -0700

    mixed letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 /* alphabet in capital and smaal letters
-*
 */
-Return :always 0.
-
 int main (void)
 {
-	char letter;
+	char(letter);
+       for (letter = 'a'; letter <= 'z'; letter++)
+       putchar (letter);
+        for (letter ='A'; letter <= 'Z'; letter++)
+	       putchar(letter);
 
-	for (letter = 'a'; letter >= 'z'; letter ++)
-		putchar(letter);
-	for (letter = 'A'; letter >= 'Z'; letter ++)
-		putchar(letter);
-
-	putchar(\n);
+	putchar('\n');
 }
 
 	
