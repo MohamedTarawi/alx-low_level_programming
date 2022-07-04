0fda3838b5fe5d86d57cb23a0b91ad71ae6b269a
MohamedTarawi <sadaqali1923@gmail.com>
2022-07-04 11:45:25 -0700

    number combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,7 +4,7 @@
 *
 *Return: always 0
 */
-int main(void);
+int main(void)
 {
 	int i;
 
@@ -13,8 +13,8 @@ int main(void);
 		putchar((i % 10) + '0');
 		
 	putchar(',');
-	putchar(' ')'
-	 }
+	putchar(' ');
+	}
 	 putchar('\n');
 	 return (0);
 } 
