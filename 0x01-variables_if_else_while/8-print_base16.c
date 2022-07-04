df31769aa498892e074a9ea88f8444941978f559
MohamedTarawi <sadaqali1923@gmail.com>
2022-07-04 10:21:52 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints base sixteen numbers
+ *
+ * return: always success
+ */
+int main(void)
+{
+	ints i;
+	{
+	for (i = 1; i<=f; i++)
+		putchar(i'%x');
+	}
+	putchar('\n');
+	return (0);
+}
