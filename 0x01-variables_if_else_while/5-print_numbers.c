04a575fe3770d529abf9e77cd4135f480d9ebc37
MohamedTarawi <sadaqali1923@gmail.com>
2022-07-04 02:25:12 -0700

    print bade 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*main - list all numbers between zero and ten
+*return:always success
+*
+*/
+int main(void)
+{
+	int(c);
+	c>=0
+	c<9
+
+	putchar(c);
+	putchar('\n')
+	return(0);
+}
