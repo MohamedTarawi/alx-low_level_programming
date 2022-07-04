c12083bce124e6e3593dae377cf12c2ca4d64343
MohamedTarawi <sadaqali1923@gmail.com>
2022-07-04 01:21:03 -0700

    except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - alphabet in lower case except e and q
+ *
+ *return: always success
+ */ 
+int main(void)
+{
+       char(letter);
+	for (letter = 'a'; letter <='z'; letter++)
+	 putchar(letter);
+	for ((leter==q)!(letter==e));
+	putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
