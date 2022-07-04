98fdf7972a0751a9937d7abbcf9cc42ce6ffe715
MohamedTarawi <sadaqali1923@gmail.com>
2022-07-03 23:28:52 -0700

    mixed alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,14 +2,14 @@
 /*
 * alphabetin upper and lower cases
 */
-int main (void)
+int main(void)
 {
 	char letter;
 
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	putchar(letter);
-	for (letter = 'A'; letter <= 'Z'; letter ++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 	putchar (letter);
 
 
