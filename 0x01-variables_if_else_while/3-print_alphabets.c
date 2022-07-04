79f604f9e2e93eb20ef0f597856d71f5763e86fc
MohamedTarawi <sadaqali1923@gmail.com>
2022-07-03 22:51:33 -0700

    mixedletters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* alphabet in capital and smaal letters
+*/
+int main (void)
+{
+	char (low);
+	char (up);
+
+	for
+	
+	       	(low = 'a'; low <= 'z'; up = 'A'; up <= 'Z'; low++)
+		putchar (low, up);
+		prinf(\n;)
+
+		return (0);
+}
+
